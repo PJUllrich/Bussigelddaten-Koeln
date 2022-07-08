ddc0d5b06069b739672f5c2537416e5dcde9d188
Peter Ullrich <peter.j.ullrich@gmail.com>
2022-07-08 09:11:08 +0200

    Some data cleanup

--- Scripts/combine_files.sh
+++ Scripts/combine_files.sh
@@ -3,13 +3,7 @@ combine () {
   local path=../Daten/$1
 
   # Combines all monthly datasets into a single, temporary file
-  awk '(NR == 1) || (FNR > 1)' $path/Monatlich/*_$1.csv > $path/tmp.csv
-
-  # Removes any empty lines
-  sed '/^$/d' $path/tmp.csv > $path/Komplett_$1.csv
-  
-  # Removes the sed backup file
-  rm $path/tmp.csv
+  awk '(NR == 1) || (FNR > 1)' $path/Monatlich/*_$1.csv > $path/Komplett_$1.csv
 }
 
 combine 2020
